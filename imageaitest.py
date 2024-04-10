94c39b7afd84d6a7f199a33d7afdf1becf69ac6f
YaroslavSamchuk <110722194+YaroslavSamchuk@users.noreply.github.com>
2024-04-10 06:03:08 +0000

    first

--- imageaitest.py
+++ imageaitest.py
@@ -1,9 +1,42 @@
 from g4f.client import Client
+from PIL import Image
+from g4f.cookies import get_cookies
+import requests # request img from web
+import shutil
+import sys
 
-client = Client()
-response = client.images.generate(
-  model="gemini",
-  prompt="a white siamese cat",
-  ...
-)
-image_url = response.data[0].url
+cookies = get_cookies(".bing.com")
+client = Client(cookies=cookies)
+print(cookies)
+prompt = input("Your prompt: ")
+err_count = 0
+def create_image(prompt, err_count):
+	try:
+		response = client.images.generate(model="dall-e-3",prompt=prompt)
+		return response
+	except Exception as e:
+		err_count += 1
+		print("Error")
+		if err_count >= 10:
+			raise e
+		create_image(prompt, err_count)
+	
+	
+try:
+    response = create_image(prompt, err_count)
+except Exception as e:
+	print(f"Count of errors: 10+, Fatal Error: {e}(check your prompt)")
+	sys.exit()
+print(response)
+print(client.images.models)
+print(response)
+image_url = response.data
+counter = 0
+for i in image_url:
+	counter += 1
+	res = requests.get(i.url, stream = True)
+	if res.status_code == 200:
+		with open(f"image{counter}.jpg",'wb') as f:
+			shutil.copyfileobj(res.raw, f)
+			print(f"Image{counter} done!")
+		
